7b30293317dfd197a95f754578e9a5f5a2904a04
Shairys <39024200+Shairys@users.noreply.github.com>
2018-05-31 20:12:24 +0200

    Zadania algorytmiczne

---
+++ nwp.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main()
+{
+    std::string A,B;
+    std::cin>> A >> B;
+    int _A = A.size()+1;
+    int _B = B.size()+1;
+    int **k = new int *[_A];
+
+    for(int i=0; i<_A; i++)
+    {
+        k[i] = new int[_B];
+        for(int j=0; j<_B; j++)
+            {
+                k[i][j]=0;
+            }
+    }
+
+    int max = 0;
+    for(int i=1; i<_A; i++)
+        for(int j=1; j<_B; j++)
+        {
+            if(A[i-1]==B[j-1] )
+            {
+                k[i][j]=k[i-1][j-1]+1;
+
+            }
+            else
+                k[i][j] = (k[i-1][j]>k[i][j-1]) ? k[i-1][j] : k[i][j-1];
+        }
+
+    std::cout<<k[_A-1][_B-1];
+
+
+
+
+}
