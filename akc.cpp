1742023d227f6e3cbef9f06cc799081e9f306d3f
Shairys <39024200+Shairys@users.noreply.github.com>
2018-07-04 11:54:41 +0200

    Revert "Zadania Algorytmiczne"
    This reverts commit 249622ca21a01024824064310e8e2cfe32e108a5.

--- akc.cpp
+++ akc.cpp
@@ -1,64 +1,70 @@
 #include <iostream>
-
-int searchDown(int * tab, int s, int n)
+#include <vector>
+using namespace std;
+int binary_search1(vector<int> & test, int x)
 {
-    int p = 0;
-    int q = n-1;
-    while(p < q)
+int p=0, k=test.size()-1;
+int srodek=0;
+while(p<k)
     {
-        int h = (p+q)/2;
-        if (tab[h] >= s)
-            q = h;
+        srodek=(p+k)/2;
+        if(test[srodek]>=x)
+            k=srodek;
         else
-            p = h+1;
+            p=srodek+1;
     }
-    if(tab[p] != s)
-        return -1;
-    else
+    if(test[p]==x)
         return p;
+    else
+        return -1;
+
 }
 
-int searchUp(int * tab, int s, int n)
+int binary_search2(vector<int> & test, int x)
 {
-    int p = 0;
-    int q = n-1;
-    while(p < q)
+int p=0, k=test.size()-1;
+int srodek=0;
+while(p<k)
     {
-        int h = (p+q+1)/2;
-        if (tab[h] <= s)
-            p = h;
+        srodek=(p+k+1)/2;
+        if(test[srodek]<=x)
+            p=srodek;
         else
-            q = h-1;
+            k=srodek-1;
     }
-    if(tab[p] != s)
-        return -1;
-    else
+    if(test[p]==x)
         return p;
-}
+    else
+        return -1;
 
+}
 
 int main()
 {
-    std::ios_base::sync_with_stdio(0);
-    std::cin.tie(0);
-
-    int n, q, s;
-    std::cin >> n;
-    int * tab = new int[n];
-
-    for(int i = 0; i < n; i++)
-        std::cin >> tab[i];
-
-    std::cin >> q;
-
-    while(q--)
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> cz;
+    int tmp, tmp2;
+    int p, k;
+    cin>>tmp;
+    while(tmp--)
     {
-        std::cin >> s;
-        int p = searchDown(tab, s, n);
-        int q = searchUp(tab, s, n);
-        if (p == -1)
-            std::cout<<0 << "\n";
+        cin>>tmp2;
+        cz.push_back(tmp2);
+    }
+    cin>>tmp;
+    while(tmp--)
+    {
+        cin>>tmp2;
+        p=binary_search1(cz, tmp2);
+        if(p<0)
+            cout<<0 <<"\n";
         else
-            std::cout<< q - p + 1 << "\n";
+        {
+            k=binary_search2(cz, tmp2);
+            cout<<k-p+1 <<"\n";
+        }
     }
 }
+
+
