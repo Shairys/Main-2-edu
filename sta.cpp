42020d5c6813e8b215189ec1afd25faf87585a57
Shairys <39024200+Shairys@users.noreply.github.com>
2018-07-20 16:09:24 +0200

    Lekcja 9

---
+++ sta.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+int tree[524289]; //2^19 +1
+int bajt = 0;
+int leaf_count;
+
+void load()
+{
+    for(int i = leaf_count; i < leaf_count+bajt; i++)
+        std::cin >> tree[i];
+    int current = leaf_count/2;
+    int tmp = leaf_count;
+    while(tmp > 1){
+        while(current < leaf_count){
+            tree[current] = std::max(tree[current*2], tree[current*2+1]);
+            current++;
+        }
+        tmp /= 2;
+        current = tmp/2;
+    }
+}
+
+void update(int k, int x)
+{
+    int current = leaf_count+k;
+    tree[current] = x;
+    current /= 2;
+    while(current >= 1){
+        tree[current] = std::max(tree[current*2], tree[current*2+1]);
+        current /= 2;
+    }
+}
+
+int max(int L, int R, int v = 1, int a = 0, int b = leaf_count - 1){
+	if (L == a && R == b) {
+		return tree[v];
+	}
+	int mid = (a + b - 1) / 2;
+	if (R <= mid) {
+		return max(L, R, 2 * v, a, mid);
+	}
+	else if (L > mid) {
+		return max(L, R, 2 * v + 1, mid + 1, b);
+	}
+	 else {
+	    return std::max(
+		max(L, mid, 2 * v, a, mid),
+		max(mid + 1, R, 2 * v + 1, mid + 1, b)
+		);
+	}
+}
+
+void display(int p = 1, int k = 1)
+{
+    for(int i = p; i <= k; i++)
+        std::cout << tree[i] << " ";
+    std::cout << "\n";
+    p*=2; k = k*2 + 1;
+    if(p <= leaf_count)
+        display(p, k);
+    else
+        std::cout << "\n\n";
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    int m, k, x;
+    std::string s;
+    std::cin >> bajt >> m;
+    int log = std::ceil(std::log2(bajt));
+    leaf_count = std::pow(2, log);
+    load();
+
+    while(m--)
+    {
+        std::cin >> s;
+        if(s == "MAX"){
+            std::cin >> k >> x;
+            std::cout << max(k - 1, x - 1) << "\n"; // Bo wnioski numerujemy od 1
+        }
+        else{
+            std::cin >> k >> x;
+            update(k - 1, x);
+        }
+    }
+}
